1ccf78782d937c4db564b1c31b4227490c321d42
Aamnah <hello@aamnah.com>
2016-12-05 11:28:29 +0500

    basic multipage app skeleton

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+import Navigation from './Navigation'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <Navigation />
+        { this.props.children } 
+      </div>
+    )
+  }
+}
